cff34b406664687fd369f8ff546224fd4f4efb8f
Nikita <fritee@mail.ru>
2016-11-05 17:10:55 +0000

    Added interface for interaction with crate

---
+++ source/goose/crateInterface.py
@@ -0,0 +1,45 @@
+from crate import client
+
+
+
+
+'''
+Create database to store geese
+'''
+def Create(crateServer = "10.79.0.230:4200"):
+    connection = client.connect(crateServer)
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE geese(
+        ID INT PRIMARY KEY,
+        HASHID VARCHAR(8),
+        LIFESPAN FLOAT(8),
+        AGE INT,
+        HUNGER FLOAT(8),
+        LOCATION_X FLOAT(16),
+        LOCATION_Y FLOAT(16),
+        HEALTH INT
+    );
+    """)
+
+'''
+Save function for geese table
+Accesses crate on ip provided and saves _geeseArray in it
+'''
+def Save(crateServer = "10.79.0.230:4200", ta, _geeseArray):
+    connection = client.connect(crateServer)
+    cursor = connection.cursor()
+    cursor.execute("""
+    #TODO
+    """);
+
+'''
+Load function for geese table
+Accesses crate on ip provided and loads it into _geeseArray
+'''
+def Load(crateServer = "10.79.0.230:4200", ta, _geeseArray):
+    connection = client.connect(crateServer)
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT * FROM geese
+    """);
