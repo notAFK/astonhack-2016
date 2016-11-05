2e9a66cf8e19ed80585a8325c6005be635c80fb9
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-05 18:43:32 +0000

    Basic input cli.

--- source/geesemanager.py
+++ source/geesemanager.py
@@ -29,26 +29,58 @@ def makeClan():
     pass
 
 
+def harvestGooseData():
+    name = str(raw_input('name ---> '))
+    age = int(raw_input('age (days) ---> '))
+    lifespan = int(raw_input('lifespan (days) ---> '))
+    health = int(raw_input('health ---> '))
+    hunger = int(raw_input('hunger ---> '))
+    print '0) Female'
+    print '1) Male'
+    print '2) Apache attack helicopter'
+    gender = int(raw_input('gender --->'))
+    location = raw_input('location (x,y) ---> ').split(',')
+    return [name, age, lifespan, health, hunger, gender, location]
+
+
 def getSelection():
     print 'Select one of the options:'
-    print '0) Generate one goose.'
-    print '1) Generate two goose.'
+    print '1) Generate one goose.'
     print '2) Generate multiple geese.'
     print '3) Exit.'
-
     INPUT = int(raw_input('---> '))
-    if INPUT == 0:
-        makeGoose()
-    elif INPUT == 1:
-        makeGoose()
-        makeGoose()
+    if INPUT == 1:
+        print 'Select one of the options:'
+        print '1) Generate a random goose.'
+        print '2) Generate a goose with custom data.'
+        INPUT = int(raw_input('---> '))
+        if INPUT == 1:
+            pass  # MAKE ONE RANDOM GOOSE AND STORE IT ...
+        elif INPUT == 2:
+            print harvestGooseData()
     elif INPUT == 2:
-        makeClan()
+        print 'How many?'
+        INPUT = int(raw_input('---> '))
+        if not type(INPUT) is int:
+            print '*quack*'
+            sys.exit()
+        else:
+            print 'Do you want random geese? (yes/no)'
+            INPUT = str(raw_input('---> '))
+            if INPUT == 'yes':
+                pass  # DO YES STUFF
+            elif INPUT == 'no':
+                pass  # DO NO STUFF
+            else:
+                print '*quack*'
+                sys.exit()
     elif INPUT == 3:
+        print 'The geese will find you!'
         sys.exit()
     else:
-        print 'WUT?'
+        print '*quack*'
         getSelection()
+
 
 if __name__ == '__main__':
     print WELCOMEMSG
