e9dd3fe251e6dae9f12223d919f9d57be0e97632
Nikita <fritee@mail.ru>
2016-11-06 09:14:05 +0000

    Partly finished website mapping and copied php file

--- source/crateTest.py
+++ source/crateTest.py
@@ -1,10 +1,12 @@
 from goose.clan import Clan
 from db.database import Database
 from goose.clan import generateRandomClan
+from geesemanager import createGeese
 
 db = Database("10.0.2.15:4200")
 db.Delete()
 db.Create()
-testClan = Clan(generateRandomClan(10, 0, 0))
+geesearray = createGeese(1000)
+testClan = Clan(geesearray)
 db.Save(testClan.geese)
 db.Save(testClan.geese)
